a97d16550f88dcab5e07f27c0331d5781bd1dad4
MunikumarIconnect <munikumarG.24b.iconnect@outlook.com>
2024-12-16 12:47:50 +0530

    layered Architecture

--- OMS/OmsMianV2.cs
+++ OMS/OmsMianV2.cs
@@ -1,11 +1,14 @@
-﻿using SampleApp;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-
+using Configuration;
+using FileModel;
+using ProjectHelper;
+using FilesEnum;
+using FileProcessses;
 namespace OMS
 {
     internal class OmsMianV2 : ConfigHelper
