ac725a4ac9716dfbaa5480ebe453a4f2b129e373
venkateshiconnect <venkatesht.24b.iconnect@outlook.com>
2024-12-18 13:18:30 +0530

    FILES CREATED

---
+++ RSC/AppConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSC.AppConnection_Kiran
+{
+    public class AppConnection
+    {
+        public static string rootFolderPath { get; set; }
+        public static string rSCConnectionString { get; set; }
+
+        static AppConnection()
+        {
+            rootFolderPath = ConfigurationManager.AppSettings["RootFolder"];
+            rSCConnectionString = ConfigurationManager.ConnectionStrings["kiranRSCConnectionString"].ToString();
+        }
+    }
+}
+        
+
---
+++ RSC/SyncStoreDataToDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSC.FileModel_Kiran;
+using RSC.AppConnection_Kiran;
+namespace DataBaseAccessLayer
+{
+    internal class SyncStoreDataToDB:AppConnection
+    {
+        private void PushStoreDataToDB(StoreModel Model)
+        {
+            using (SqlConnection con = new SqlConnection(rSCConnectionString))
+            {
+                string query = $"Update Stores Set StoreName = @StoreName, Location = @Location, ManagerName= @Manager, ContactNumber = @ContactNumber where StoreCode = @StoreCode";
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = query;
+                    cmd.Connection = con;
+
+                    cmd.Parameters.Add("@StoreName", DbType.String).Value = Model.storeName;
+                    cmd.Parameters.Add("@StoreCode", DbType.String).Value = Model.storeCode;
+                    cmd.Parameters.Add("@Location", DbType.String).Value = Model.location;
+                    cmd.Parameters.Add("@Manager", DbType.String).Value = Model.managerName;
+                    cmd.Parameters.Add("@ContactNumber", DbType.String).Value = Model.contactNumber;
+                    con.Close();
+                }
+            }
+
+        }
+    }
+}
