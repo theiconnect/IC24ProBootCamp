f38591585e8c8598d4921e3607603fe93bb25a55
Yuvaiconnect24A <yuvag.24a.iconnect@outlook.com>
2024-12-23 13:13:23 +0530

    TEST

--- RSCDataAccess/StoreDataAcess.cs
+++ RSCDataAccess/StoreDataAcess.cs
@@ -22,6 +22,7 @@ namespace RSCDataAccess
                 {
                     connection.Open();
                     using (SqlDataReader reader = cmd.ExecuteReader())
+                        //Add comment
                     {
                         while (reader.Read())
                         {
