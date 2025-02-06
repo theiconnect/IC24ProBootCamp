d683e09a000890cd739f025376109780317592e7
krishnaveniiconnect <krishnavenit.24b.iconnect@outlook.com>
2025-02-06 10:44:20 +0530

    Login Layout Added

---
+++ RMSNextGen/RMSNextGen/RMSNextGen.Web/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMSNextGen.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SendOtp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VerfyOtp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
