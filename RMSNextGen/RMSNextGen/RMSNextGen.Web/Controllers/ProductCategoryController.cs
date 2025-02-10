cb21c7de4e4704a22bb639eee1f229fcdc5bff73
VijayIconnect <vijayG.24b.iconnect@outlook.com>
2025-02-10 12:44:34 +0530

    I have changed productcategory

---
+++ RMSNextGen/RMSNextGen/RMSNextGen.Web/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMSNextGen.Web.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryList()
+        {
+            return View();
+        }
+        public IActionResult EditCategory()
+        {
+            return View();
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        public IActionResult ViewCategory()
+        {
+            return View();
+        }
+
+
+    }
+}
