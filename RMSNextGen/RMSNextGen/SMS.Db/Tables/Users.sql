f079ad14fc1d41d5df61dd2dee5cb178d8a1aeb8
Murali <theiconnect.org@gmail.com>
2025-02-10 20:21:47 +0530

    DB imported

---
+++ RMSNextGen/RMSNextGen/SMS.Db/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]       INT           IDENTITY (1, 1) NOT NULL,
+    [Email]        VARCHAR (512) NOT NULL,
+    [PasswordHash] VARCHAR (255) NOT NULL,
+    [RoleId]       INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
