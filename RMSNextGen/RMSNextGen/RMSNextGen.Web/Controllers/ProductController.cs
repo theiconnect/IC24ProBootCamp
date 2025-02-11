3ef8211ec6e2ed18ad025157748c9655c4ca3d6f
krishnaveniiconnect <krishnavenit.24b.iconnect@outlook.com>
2025-02-11 18:03:53 +0530

    changed
    changed

--- RMSNextGen/RMSNextGen/RMSNextGen.Web/Controllers/ProductController.cs
+++ RMSNextGen/RMSNextGen/RMSNextGen.Web/Controllers/ProductController.cs
@@ -4,29 +4,59 @@ namespace RMSNextGen.Web.Controllers
 {
     public class ProductController : Controller
     {
+        [HttpGet]
         public IActionResult ProductList()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult SearchProducts()
+        public IActionResult ProductList(IFormCollection form)
         {
-            //perform search operation
-            //filter the list
-            //
-            return RedirectToAction("ProductList");
+            
+            return RedirectToAction("ProductList","Product");
         }
+        [HttpGet]
         public IActionResult AddNewProduct()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult AddNewProduct(IFormCollection form)
+        {
+            return RedirectToAction("ProductList", "Product");
+
+            
+        }
+        [HttpGet]
         public IActionResult EditProduct()
         {
             return View();
-        } 
+        }
+        [HttpPost]
+        public IActionResult EditProduct(IFormCollection form)
+        {
+            return RedirectToAction("ProductList", "Product");
+
+
+        }
+        [HttpGet]
         public IActionResult ViewProduct()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Save(IFormCollection form)
+        {
+            return RedirectToAction("ProductList", "Product");
+
+
+        }
+        [HttpPost]
+        public IActionResult Search(IFormCollection form)
+        {
+            return RedirectToAction("ProductList", "Product");
+
+
         }
     }
 }
