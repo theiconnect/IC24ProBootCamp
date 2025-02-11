a3aa55896f9f4b3b12ee2e73da317b088f8e16c6
kiraniconnect <kirank.24b.iconnect@outlook.com>
2025-02-11 18:11:38 +0530

    I have changed customer page and navigestations

--- RMSNextGen/RMSNextGen/RMSNextGen.Web/Controllers/CustomersController.cs
+++ RMSNextGen/RMSNextGen/RMSNextGen.Web/Controllers/CustomersController.cs
@@ -4,21 +4,54 @@ namespace RMSNextGen.Web.Controllers
 {
     public class CustomersController : Controller
     {
+        [HttpGet]
         public IActionResult CustomerList()
         {
             return View();
         }
-        public IActionResult AddNewCustomer()
+        [HttpPost]
+		public IActionResult CustomerList(IFormCollection form)
+		{
+			return RedirectToAction("CustomerList", "Customers");
+		}
+
+
+
+		public IActionResult AddNewCustomer()
         {
             return View();
         }
+
         public IActionResult EditCustomer()
         {
             return View();
         }
-        public IActionResult ViewCustomer()
+        [HttpGet]
+		public IActionResult Save()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Save(IFormCollection form)
+		{
+			return RedirectToAction("CustomerList", "Customers");
+		}
+
+
+		public IActionResult ViewCustomer()
         {
             return View();
         }
-    }
+		[HttpGet]
+		public IActionResult Back()
+		{
+			return RedirectToAction("CustomerList", "Customers");
+		}
+		
+		[HttpGet]
+		public IActionResult Search()
+		{
+			return View();
+		}
+	}
 }
