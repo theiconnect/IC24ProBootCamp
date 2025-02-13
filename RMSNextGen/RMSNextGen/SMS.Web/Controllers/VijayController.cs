fa51a2d80e865b7f6f00bd75590173cce60b7c30
Vijayiconnect24A <vijaykumarch.24a.iconnect@outlook.com>
2025-02-13 09:21:27 +0530

    .

---
+++ RMSNextGen/RMSNextGen/SMS.Web/Controllers/VijayController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS.Web.Controllers
+{
+    public class VijayController : Controller
+    {
+        [HttpGet]
+        public IActionResult StudentRegistration()
+        {
+            return View();
+        }
+    }
+}
