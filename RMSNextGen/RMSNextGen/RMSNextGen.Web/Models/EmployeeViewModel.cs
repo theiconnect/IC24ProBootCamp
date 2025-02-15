1c4473511886e3fe901145cb70dfd29f516c8e29
Saikumariconnect24B <saikumarp.24b.iconnect@outlook.com>
2025-02-15 13:53:21 +0530

    Added employeeviewmodel.

---
+++ RMSNextGen/RMSNextGen/RMSNextGen.Web/Models/EmployeeViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RMSNextGen.Web.Models
+{
+	public class EmployeeViewModel
+	{
+		public string EmployeeCode { get; set; }
+		public string EmployeeName { get; set; }
+		public string StoreCode { get; set; }
+		public string Role { get; set; }
+		public string Gender { get; set; }
+		public string Salary { get; set; }
+		public string ContactNumber { get; set; }
+
+		public string Addressline1 { get; set; }
+		public string Addressline2 { get; set; }
+
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Pincode { get; set; }
+	}
+}
