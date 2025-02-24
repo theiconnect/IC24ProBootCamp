f5dfd2289f0c98feaf96f5e72f2c0d9be58b176b
krishnaveniiconnect <krishnavenit.24b.iconnect@outlook.com>
2025-02-24 17:19:31 +0530

    Validation started

--- RMSNextGen/RMSNextGen/RMSNextGen.Web/Models/ProductViewModel.cs
+++ RMSNextGen/RMSNextGen/RMSNextGen.Web/Models/ProductViewModel.cs
@@ -1,11 +1,24 @@
-﻿namespace RMSNextGen.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RMSNextGen.Web.Models
 {
 	public class ProductViewModel
 	{
+		[Required(ErrorMessage="ProductCode is required")]
+		[Display(Name = "ProductCode")]
 		public string ProductCode { get; set; }
+
+		[Required(ErrorMessage ="ProductName is required")]
+		[Display(Name = "ProductName")]
 		public string ProductName { get; set; }
 		public string Category { get; set; }
+
+		[Required(ErrorMessage = "PricePerUnit is required")]
+		[Display(Name = "PricePerUnit")]
 		public string PricePerUnit { get; set; }
+
+		[Required(ErrorMessage = "ThresholdLimit is required")]
+		[Display(Name = "ThresholdLimit")]
 		public string ThresholdLimit { get; set; }
 		public string UnitofMeasurement { get; set; }
 		public string CreatedBy {  get; set; }
